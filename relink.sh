fc17e744e5575c888724a023d48dd2cc21f81e18
Vadim Fint <mocksoul@gmail.com>
2013-02-28 02:59:47 +0400

    vim: move ~/.vim -> ~/.config/vim. Proper way.

--- relink.sh
+++ relink.sh
@@ -4,6 +4,6 @@ P=$(realpath ${0%/*})
 H=$HOME
 
 ln -sfn $P/vim/vimrc $H/.vimrc
-ln -sfn $P/vim/vim $H/.vim
+ln -sfn $P/vim/vim $H/.config/vim
 ln -sfn $P/powerline $H/.config/powerline
 ln -sfn $P/bin/peat $H/bin/peat
