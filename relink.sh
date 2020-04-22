5c15bd907fd44e34bae2bf2332e4e3500b781c8b
Vadim Fint <mocksoul@gmail.com>
2020-04-22 20:55:30 +0300

    alacritty: added

--- relink.sh
+++ relink.sh
@@ -10,3 +10,5 @@ ln -sfn $P/vim/vimrc $H/.vimrc
 ln -sfn $P/vim/vim $H/.config/vim
 ln -sfn $H/.vimrc $H/.config/nvim/init.vim
 ln -sfn $H/.local/share/nvim/view $H/.vim/view
+
+ln -sfn $P/alacritty $H/.config/alacritty
