848d924c89fbf03783e72b84e13c3d1fc137becf
Vadim Fint <mocksoul@gmail.com>
2020-04-22 20:56:00 +0300

    tmux: added

--- relink.sh
+++ relink.sh
@@ -11,4 +11,5 @@ ln -sfn $P/vim/vim $H/.config/vim
 ln -sfn $H/.vimrc $H/.config/nvim/init.vim
 ln -sfn $H/.local/share/nvim/view $H/.vim/view
 
+ln -sfn $P/tmux/tmux.conf $H/.tmux.conf
 ln -sfn $P/alacritty $H/.config/alacritty
