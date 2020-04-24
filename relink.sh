31bce3ec62b0d902e08f158ea7e8cec6f027eeee
Vadim Fint <mocksoul@gmail.com>
2020-04-24 05:49:25 +0300

    zsh: added zshrc

--- relink.sh
+++ relink.sh
@@ -4,9 +4,14 @@ P=$(realpath ${0%/*})
 H=$HOME
 
 mkdir -p $H/.local/share/nvim/{plugged,shada,swap,view}
+mkdir -p $H/.local/share/zsh
 
-ln -sfn $P/vim $H/.config/vim
-ln -sfn $H/.config/vim/vimrc $H/.vimrc
+ln -sfn $P/zsh $H/.config/zsh
+ln -sfn $H/.config/zsh/zshrc $H/.zshrc
+
+ln -sfn $P/vim/vimrc $H/.vimrc
+ln -sfn $P/vim/vim $H/.config/vim
+
 ln -sfn $H/.local/share/nvim/view $H/.vim/view
 
 ln -sfn $P/tmux/tmux.conf $H/.tmux.conf
@@ -15,3 +20,7 @@ ln -sfn $P/alacritty $H/.config/alacritty
 ln -sfn $P/bin/alacritty_quake_toggle $H/.local/bin
 
 ln -sfn $P/kde/khotkeysrc $H/.config
+
+# Checkout zsh plugins if we didnt do that yet
+[ ! -d $H/.local/share/zsh/plug/wakatime ] && git clone https://github.com/sobolevn/wakatime-zsh-plugin.git $H/.local/share/zsh/plug/wakatime
+
