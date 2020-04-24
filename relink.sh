847e1288ff04143d4c6d5e3af3ff0e0d0d833fb2
Vadim Fint <mocksoul@gmail.com>
2020-04-24 05:48:40 +0300

    vim: do not use .config/nvim at all, change base dir

--- relink.sh
+++ relink.sh
@@ -3,12 +3,10 @@
 P=$(realpath ${0%/*})
 H=$HOME
 
-mkdir -p $H/.config/nvim
 mkdir -p $H/.local/share/nvim/{plugged,shada,swap,view}
 
-ln -sfn $P/vim/vimrc $H/.vimrc
-ln -sfn $P/vim/vim $H/.config/vim
-ln -sfn $H/.vimrc $H/.config/nvim/init.vim
+ln -sfn $P/vim $H/.config/vim
+ln -sfn $H/.config/vim/vimrc $H/.vimrc
 ln -sfn $H/.local/share/nvim/view $H/.vim/view
 
 ln -sfn $P/tmux/tmux.conf $H/.tmux.conf
